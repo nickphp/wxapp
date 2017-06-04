d87660b3125b1be2fb61130db36d0ea12c579c7c
nickphp <51153627@qq.com>
2017-06-04 15:21:41 +0800

    123

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(function(req, res, next) {
     res.send("404 Not Find");
 //  var err = new Error('Not Found');
 //  err.status = 404;
-//  next(err);//3233//ll//
+//  next(err);//3233//ll//123
 
 });
 // error handler
